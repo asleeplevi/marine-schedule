1c7a3276a38bd2f734378f1e61b0608b03ab465a
asleeplevi <jlevicarvalho@gmail.com>
2022-11-08 22:45:22 -0300

    style: added css reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-
+import { CssBaseline } from '@mui/material'
 import { router } from './routes'
 import './ipc/node-api'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
