cb3972cda75fff82e837388a118c238ee836624c
asleeplevi <jlevicarvalho@gmail.com>
2022-11-09 14:15:19 -0300

    feat: list all organizations route

--- src/ipc/functions/getOrganizations.ts
+++ src/ipc/functions/getOrganizations.ts
@@ -1,3 +1,26 @@
-export function getOrganizations() {
-  return { status: "success", data: [] };
+import { ApiResponseProps } from '@/types/api'
+
+export const FAKE_ORGANIZATIONS = [
+  {
+    name: 'Capitania 1',
+    nidom: '1',
+  },
+  {
+    name: 'Capitania 2',
+    nidom: '2',
+  },
+  {
+    name: 'Capitania 3',
+    nidom: '3',
+  },
+  {
+    name: 'Capitania 4',
+    nidom: '4',
+  },
+]
+
+export function getOrganizations(): ApiResponseProps<
+  typeof FAKE_ORGANIZATIONS
+> {
+  return { status: 'success', data: FAKE_ORGANIZATIONS, isCached: false }
 }
